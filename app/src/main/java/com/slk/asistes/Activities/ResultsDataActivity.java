06ede7ccce527dbd185576d95ce2733ad1c428c3
Igor Vaganov <i@asistes.com>
2015-10-19 21:54:27 +0300

    Обработка нажатий на айтемы и открытие нового активити

--- app/src/main/java/com/slk/asistes/Activities/ResultsDataActivity.java
+++ app/src/main/java/com/slk/asistes/Activities/ResultsDataActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -19,6 +20,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ResultsDataActivity extends AppCompatActivity {
+
+    public interface ProductItemClickProcess {
+        public void TryProcess(JSONObject result);
+    }
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -55,12 +60,14 @@ public class ResultsDataActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        // specify an adapter (see also next example)
-        String[] arr = new String[3];
-        arr[0]="1";
-        arr[1]="1";
-        arr[2]="1";
-        mAdapter = new ProductsAdapter(productsArray);
+        mAdapter = new ProductsAdapter(productsArray, new ProductItemClickProcess() {
+            @Override
+            public void TryProcess(JSONObject result) {
+                Intent productCardIntent = new Intent(ResultsDataActivity.this, ProductCardActivity.class);
+                productCardIntent.putExtra("product", result.toString());
+                startActivity(productCardIntent);
+            }
+        });
         mRecyclerView.setAdapter(mAdapter);
 
 
@@ -78,6 +85,8 @@ public class ResultsDataActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
 
 }
 
--- app/src/main/java/com/slk/asistes/Adapters/ProductsAdapter.java
+++ app/src/main/java/com/slk/asistes/Adapters/ProductsAdapter.java
@@ -1,6 +1,7 @@
 package com.slk.asistes.Adapters;
 
 import android.net.Uri;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +9,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.slk.asistes.Activities.ResultsDataActivity;
 import com.slk.asistes.R;
+import com.slk.asistes.Static.Logger;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,23 +24,45 @@ import org.json.JSONException;
 public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ViewHolder> {
     private JSONArray mDataset;
 
+    private ResultsDataActivity.ProductItemClickProcess parentCallback;
+
+    private interface ItemClickListener {
+        void onClick(CardView view, int position, boolean isLongClick);
+    }
+
     // Provide a reference to the views for each data product_item
     // Complex data items may need more than one view per product_item, and
     // you provide access to all the views for a data product_item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         // each data product_item is just a string in this case
         public TextView productLabel;
         public ImageView productImage;
+        public CardView currentCardView;
+        private ItemClickListener clickListener;
+
+
         public ViewHolder(View itemView) {
             super(itemView);
+
+            itemView.setOnClickListener(this);
+            currentCardView = (CardView) itemView.findViewById(R.id.product_card_item);
             productLabel = (TextView)itemView.findViewById(R.id.card_label);
             productImage = (ImageView)itemView.findViewById(R.id.card_image);
+        }
+
+        public void setClickListener(ItemClickListener itemClickListener) {
+            this.clickListener = itemClickListener;
+        }
+        @Override
+        public void onClick(View view) {
+            clickListener.onClick(currentCardView, getAdapterPosition(), false);
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public ProductsAdapter(JSONArray myDataset) {
+    public ProductsAdapter(JSONArray myDataset, ResultsDataActivity.ProductItemClickProcess clickCallbackRef) {
         mDataset = myDataset;
+        parentCallback = clickCallbackRef;
     }
 
     // Create new views (invoked by the layout manager)
@@ -44,8 +70,7 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ViewHo
     public ProductsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                    int viewType) {
         // create a new view
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.product_item, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item, parent, false);
         // set the view's size, margins, paddings and layout parameters
        // ...
         ViewHolder vh = new ViewHolder((LinearLayout)v);
@@ -67,7 +92,18 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ViewHo
         }
 
         holder.productLabel.setText(name);
+        holder.setClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(CardView view, int position, boolean isLongClick) {
+                try {
 
+                    parentCallback.TryProcess(mDataset.getJSONObject(position));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
     //    holder.productImage.setImageURI("http://stage.asistes.com");
 
