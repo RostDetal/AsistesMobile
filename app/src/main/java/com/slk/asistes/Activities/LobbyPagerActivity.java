1106344b62792e2bf6ab05fab20403e90c5b92dc
Igor Vaganov <i.vaganov@socialquantum.ru>
2015-10-21 09:45:05 +0300

    новый тулбар

--- app/src/main/java/com/slk/asistes/Activities/LobbyPagerActivity.java
+++ app/src/main/java/com/slk/asistes/Activities/LobbyPagerActivity.java
@@ -1,11 +1,11 @@
 package com.slk.asistes.Activities;
 
-import android.app.ActionBar;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -21,9 +21,6 @@ public class LobbyPagerActivity  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
-
-        TabLayout tabs = (TabLayout) findViewById(R.id.tabs_container);
-
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
